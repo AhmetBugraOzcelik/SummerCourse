1b0520e0ec4bdda3eb50838a1944dc5e300742f2
AhmetBugraOzcelik <ahmetbugraozcelik45@gmail.com>
2022-07-29 09:55:33 +0300

    Sliders Finished

--- Episode8/Js/app.js
+++ Episode8/Js/app.js
@@ -9,29 +9,83 @@
 //     variableWidth: true
 //   });
 
-  $('#boxs-dersler').slick({
-    dots: true,
-    centerMode: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          arrows: true,
-          dots: false,
-          centerMode: true,
-          slidesToShow: 3
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: true,
-          dots: false,
-          centerMode: true,
-          slidesToShow: 1
-        }
+$('.boxs').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+        arrows :true
       }
-    ]
-  });
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        arrows :false,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows :false,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+$('#boxs-dersler').slick({
+  dots: true,
+  infinite: true,
+  centerMode: true,
+  arrows :false,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 923,
+      settings: {
+        dots: false,
+        arrows :true,
+        centerMode: false,
+        slidesToShow: 2,
+      }
+    },
+    {
+      breakpoint: 500,
+      settings: {
+        dots: false,
+        infinite: false,
+        arrows :true,
+        centerMode: false,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+$('.main-comment').slick({
+  arrows: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  adaptiveHeight: true
+});
--- deneme/app.js
+++ deneme/app.js
@@ -1,9 +1,30 @@
 $('.boxs').slick({
-    dots: true,
+  dots: true,
     arrows: true,
     infinite: true,
     speed: 300,
     slidesToShow: 1,
     centerMode: true,
-    variableWidth: true
-  });
+    variableWidth: true,
+  responsive: [
+    {
+      breakpoint: 500,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+        arrows:true
+      }
+    }
+  ]
+});
+
+  //   dots: true,
+  //   arrows: true,
+  //   infinite: true,
+  //   speed: 300,
+  //   slidesToShow: 1,
+  //   centerMode: true,
+  //   variableWidth: true
+  // })
